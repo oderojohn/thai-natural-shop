d54f29ec8cd4a5938955dbf8517826804ccfb517
john odero <john.odero26@gmail.com>
2023-03-28 21:20:27 +0300

    getting ready for production

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-95t%=#4o3@l-(-%ok9*h%n3!0(sdchjn%+_$5#umaj-!3bg*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["oderojohn.pythonanywhere.com", '127.0.0.1']
 
 
 # Application definition
@@ -120,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static'
+
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
